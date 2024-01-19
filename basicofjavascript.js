49bf3b2a553242186b148ec7814c053cc52686f5
Manthan Raut <106598373+yesmanthan@users.noreply.github.com>
2024-01-19 23:17:56 +0530

    Add files via upload

---
+++ basicofjavascript.js
@@ -0,0 +1,26 @@
+//FIRST WE LEARN ABOUT VARIABLES
+//THERE ARE  3 TYPES OF VARIABLES VAR,LET & COST
+//LET= WE JUST GAVE THE VALUE TO LWT OS JUST ASSUME IT (LIKELY TO ME MORE USED  THAN VAR)
+//CONST = IN CONSTANT IF EW CHANGE THE VALUE IT WILL THROW THE ERROR
+//VAR =IT  JUST STANDS FOR VARIABLE LIKE  LER  BUT LET HAS MORE TEST CASES THAN VAR 
+ /// LETS EXPERIMENT IT 
+ 
+ let a=100;
+ console.log(a);
+ let b=200;
+ b=250;
+console.log(b);
+
+var c=300;
+console.log(c);
+var d=400;
+d=450;
+console.log(d);
+
+const e=500;
+console.log(e);
+const f=600;
+//f=650;//changing the value the error will like be TypeError: Assignment to constant variable.
+console.log(f);
+
+
